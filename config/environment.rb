e259ec9ce9dc54078df5f1a3e31471a1d3b047b3
ysorigin <ysorigin@gmail.com>
2009-12-28 11:23:47 +0800

    init lighthouse-api config

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,10 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
+  Lighthouse.account = 'theplant'
+  Lighthouse.token = "5da87166315902951c3dfbb27ff4c6e75ee65b4d"
 
+  config.gem "lighthouse-api", :lib => false
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
