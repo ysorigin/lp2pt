a0f84d7d0fe2b3535addfc5c38ad415cd619de14
ysorigin <ysorigin@gmail.com>
2009-12-28 13:03:18 +0800

    link to lp ticket

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def ticket_url(p, t)
+    "https://theplant.lighthouseapp.com/projects/#{p.id}/tickets/#{t.id}-#{t.permalink}"
+  end
 end
