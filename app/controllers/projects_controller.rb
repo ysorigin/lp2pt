0c7ac86f9fc3b3165f15a2fb350ef4a94654fafd
ysorigin <ysorigin@gmail.com>
2009-12-28 12:03:53 +0800

    add project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Lighthouse::Project.find(:all)
   end
+
+  def show
+    @project = Lighthouse::Project.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => "projects", :action => :index
 
+  map.resource :project
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
